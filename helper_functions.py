4e88b6d8fda45c1b42a9c92f3a155818d0f4ac13
Dominik Polzer <dominik.polzer@siemens.com>
2020-11-17 21:01:51 +0100

    defined table with all working days

--- helper_functions.py
+++ helper_functions.py
@@ -1,13 +1,11 @@
-import smtplib, ssl
 import mysql.connector
-import imaplib
-import pprint
-import os
-from toggl.TogglPy import Toggl
-import requests
 import base64
-from datetime import date
+from datetime import date, timedelta
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import re
+from datetime import datetime
 
 def connect_to_database(password,schema,user='root',port='3308',host='127.0.0.1'):
     '''Connects to mysql database'''
@@ -66,6 +64,9 @@ def get_all_time_entries(headers, start_date):
     return time_entries
 
 def data_processing(clients,projects,time_entries):
+    '''Join clients, projects and time entries to a data frame with all time entries
+    and the corresponding information to clients and projects'''
+
     projects_filtered = [{'pid': item['id'],
                           'cid': item['cid'],
                           'project_name': item['name']} for item in projects]
@@ -82,4 +83,194 @@ def data_processing(clients,projects,time_entries):
 
     return time_entries_extended
 
-def web_scraper_puplic_holidays():
+def define_working_days_table(start_date = date(2020, 8, 17), end_date = date.today()):
+
+    def web_scraper_puplic_holidays():
+        '''
+        The following code retrieves the source code from https://www.ferienwiki.de/feiertage/de/bayern and
+        saves the german (bavarian) puplic holidays in a MySQL database.
+        :return:puplic_holidays_df: (data frame with a entry for each puplic holiday
+                                    in bavaria)
+        '''
+        url = 'https://www.ferienwiki.de/feiertage/de/bayern'
+        response = requests.get(url)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        td = soup.findAll('td')
+
+        puplic_holidays = []
+
+        for line in td:
+            try:
+                match = re.search(r'\d{2}.\d{2}.\d{4}', str(line))
+                date = datetime.strptime(match.group(), '%d.%m.%Y').date()
+                puplic_holidays.append(date)
+            except:
+                pass
+
+        puplic_holidays_df = pd.DataFrame(data=puplic_holidays)
+        return puplic_holidays_df.rename(columns={0: "days"})
+
+    puplic_holidays_df = web_scraper_puplic_holidays()
+
+    all_days = []
+    for n in range(int((end_date - start_date).days)):
+        day = start_date + timedelta(n)
+        all_days.append(day)
+
+    print(f"Number of days between start and end date: {len(all_days)}")
+
+    workdays_index = [0, 1, 2, 3, 4]
+    workdays = []
+    for day in all_days:
+        if date.weekday(day) in workdays_index:
+            workdays.append(day)
+
+    print(f"Number of workdays between start and end date: {len(workdays)}")
+
+    workdays_df = pd.DataFrame(data=workdays)
+    workdays_df = workdays_df.rename(columns={0: "days"})
+
+    def anti_join(x, y, on):
+        """Return rows in x which are not present in y"""
+        ans = pd.merge(left=x, right=y, how='left', indicator=True, on=on)
+        ans = ans.loc[ans._merge == 'left_only', :].drop(columns='_merge')
+        return ans
+
+    workdays_without_puplic_holidays_df = anti_join(workdays_df, puplic_holidays_df, on="days")
+
+    print(f"Number of workdays between start and end date (minus puplic holidays): {len(workdays_without_puplic_holidays_df)}")
+
+    return workdays_without_puplic_holidays_df
+
+def input_vacation_days():
+    pass
+
+
+def write_toggl_data_in_database(cursor, cnx, time_entries_extended):
+    return_messages=[]
+    try:
+        cursor.execute("CREATE TABLE `dashboard`.`toggl_time_entries` ("
+                       "`id` INT NOT NULL,"
+                       "`start` DATETIME NULL,"
+                       "`stop` DATETIME NULL,"
+                       "`duration` INT NULL,"
+                       "`description` VARCHAR(45) NULL,"
+                       "`project_name` VARCHAR(45) NULL,"
+                       "`client_name` VARCHAR(45) NULL,"
+                       "PRIMARY KEY (`id`));")
+        cnx.commit()
+    except mysql.connector.Error as e:
+        return_messages.append("Error code:" + str(e.errno))
+        return_messages.append("SQLSTATE value:" + str(e.sqlstate))
+        return_messages.append("Error message:" + str(e.msg))
+        return_messages.append("Error:" + str(e))
+
+        try:
+            cursor.execute("DROP TABLE `dashboard`.`toggl_time_entries`")
+            return_messages.append("Current table toggl_time_entries was deleted successfully")
+        except:
+            return_messages.append("Error while deleting table toggl_time_entries")
+
+        try:
+            cursor.execute("CREATE TABLE `dashboard`.`toggl_time_entries` ("
+                           "`id` INT NOT NULL,"
+                           "`start` DATETIME NULL,"
+                           "`stop` DATETIME NULL,"
+                           "`duration` INT NULL,"
+                           "`description` VARCHAR(45) NULL,"
+                           "`project_name` VARCHAR(45) NULL,"
+                           "`client_name` VARCHAR(45) NULL,"
+                           "PRIMARY KEY (`id`));")
+            cnx.commit()
+            return_messages.append("Table toggl_time_entries was created successfully")
+        except:
+            return_messages.append("Error while creating table toggl_time_entries")
+
+    # Create a new record
+    sql = "INSERT INTO `toggl_time_entries` (`id`, `start`, `stop`, `duration`, `description`, `project_name`, `client_name`) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    for index, line in time_entries_extended.iterrows():
+        # import pdb; pdb.set_trace()
+        if int(line['duration']) > 0:
+            try:
+                cursor.execute(sql, (line['id'],
+                                     line['start'],
+                                     line['stop'],
+                                     line['duration'],
+                                     line['description'],
+                                     line['project_name'],
+                                     line['client_name']))
+                cnx.commit()
+            except mysql.connector.Error as e:
+                return(return_messages.append("Fail during ADDING ROWS to table toggl_time_entries"))
+                return_messages.append("Error code:" + str(e.errno))
+                return_messages.append("SQLSTATE value:" + str(e.sqlstate))
+                return_messages.append("Error message:" + str(e.msg))
+                return_messages.append("Error:" + str(e))
+
+    return return_messages
+
+def write_puplic_holidays_in_database(cursor, cnx, time_entries_extended):
+    return_messages=[]
+
+    try:
+        cursor.execute("CREATE TABLE `dashboard`.`toggl_time_entries` ("
+                       "`id` INT NOT NULL,"
+                       "`start` DATETIME NULL,"
+                       "`stop` DATETIME NULL,"
+                       "`duration` INT NULL,"
+                       "`description` VARCHAR(45) NULL,"
+                       "`project_name` VARCHAR(45) NULL,"
+                       "`client_name` VARCHAR(45) NULL,"
+                       "PRIMARY KEY (`id`));")
+        cnx.commit()
+    except mysql.connector.Error as e:
+        return_messages.append("Error code:" + str(e.errno))
+        return_messages.append("SQLSTATE value:" + str(e.sqlstate))
+        return_messages.append("Error message:" + str(e.msg))
+        return_messages.append("Error:" + str(e))
+
+        try:
+            cursor.execute("DROP TABLE `dashboard`.`toggl_time_entries`")
+            return_messages.append("Current table toggl_time_entries was deleted successfully")
+        except:
+            return_messages.append("Error while deleting table toggl_time_entries")
+
+        try:
+            cursor.execute("CREATE TABLE `dashboard`.`toggl_time_entries` ("
+                           "`id` INT NOT NULL,"
+                           "`start` DATETIME NULL,"
+                           "`stop` DATETIME NULL,"
+                           "`duration` INT NULL,"
+                           "`description` VARCHAR(45) NULL,"
+                           "`project_name` VARCHAR(45) NULL,"
+                           "`client_name` VARCHAR(45) NULL,"
+                           "PRIMARY KEY (`id`));")
+            cnx.commit()
+            return_messages.append("Table toggl_time_entries was created successfully")
+        except:
+            return_messages.append("Error while creating table toggl_time_entries")
+
+    # Create a new record
+    sql = "INSERT INTO `toggl_time_entries` (`id`, `start`, `stop`, `duration`, `description`, `project_name`, `client_name`) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    for index, line in time_entries_extended.iterrows():
+        # import pdb; pdb.set_trace()
+        if int(line['duration']) > 0:
+            try:
+                cursor.execute(sql, (line['id'],
+                                     line['start'],
+                                     line['stop'],
+                                     line['duration'],
+                                     line['description'],
+                                     line['project_name'],
+                                     line['client_name']))
+                cnx.commit()
+            except mysql.connector.Error as e:
+                return(return_messages.append("Fail during ADDING ROWS to table toggl_time_entries"))
+                return_messages.append("Error code:" + str(e.errno))
+                return_messages.append("SQLSTATE value:" + str(e.sqlstate))
+                return_messages.append("Error message:" + str(e.msg))
+                return_messages.append("Error:" + str(e))
+
+    return return_messages
+
--- updatedata.py
+++ updatedata.py
@@ -1,7 +1,9 @@
 from helper_functions import connect_to_database, connect_to_toggl, \
-    get_all_clients_and_projects, get_all_time_entries, data_processing
+    get_all_clients_and_projects, get_all_time_entries, data_processing, \
+    define_working_days_table, write_toggl_data_in_database
 import os
-import pandas as pd
+import sqlalchemy
+import mysql.connector
 import pprint
 
 cnx, cursor = connect_to_database(password=str(os.environ['MYSQL_SECRET']), schema='dashboard')
@@ -12,6 +14,19 @@ clients, projects = get_all_clients_and_projects(my_workspace, headers)
 
 time_entries = get_all_time_entries(headers, start_date='2020-08-01')
 
-data_processing(clients, projects, time_entries)
-
+#process the information
 time_entries_extended = data_processing(clients, projects, time_entries)
+#fill NaN fields with "-"
+time_entries_extended = time_entries_extended.fillna("-")
+
+return_messages = write_toggl_data_in_database(cursor, cnx, time_entries_extended)
+for line in return_messages:
+    print(line)
+
+workdays = define_working_days_table()
+
+cnx.close()
+
+
+
+
